37be48afe72e987f5268188d4e98422e2f28ffce
Bittu <bittukumarthakur@users.noreply.github.com>
2023-09-11 16:15:07 +0530

    #5|bittu/biswa|add an account entity to manage stocks, tiles, and balance

---
+++ src/models/account.js
@@ -0,0 +1,37 @@
+class Account {
+  #tiles;
+  #stocks;
+  #balance;
+
+  constructor(balance = 0) {
+    this.#tiles = [];
+    this.#stocks = {};
+    this.#balance = balance;
+  }
+
+  stats() {
+    return {
+      tiles: [...this.#tiles],
+      stocks: { ...this.#stocks },
+      balance: this.#balance,
+    };
+  }
+
+  credit(amount) {
+    this.#balance += amount;
+  }
+
+  debit(amount) {
+    this.#balance -= amount;
+  }
+
+  addTile(tile) {
+    this.#tiles.push(tile);
+  }
+
+  removeTile(selectedTile) {
+    this.#tiles = this.#tiles.filter(tile => tile !== selectedTile);
+  }
+}
+
+module.exports = { Account };
---
+++ test/models/account-test.js
@@ -0,0 +1,81 @@
+const { it, describe } = require("node:test");
+const assert = require("assert");
+const { Account } = require("../../src/models/account");
+
+describe("Account", () => {
+  describe("stats", () => {
+    it("should have an initial amount of 0", () => {
+      const account = new Account();
+      const { balance } = account.stats();
+
+      assert.strictEqual(balance, 0);
+    });
+
+    it("shouldn't have any tiles initially", () => {
+      const account = new Account();
+      const { tiles } = account.stats();
+
+      assert.strictEqual(tiles.length, 0);
+    });
+
+    it("shouldn't have any stocks initially", () => {
+      const account = new Account();
+      const { stocks } = account.stats();
+
+      assert.deepStrictEqual({}, stocks);
+    });
+
+    it("should include an overall account statistic", () => {
+      const account = new Account();
+
+      assert.deepStrictEqual(account.stats(), {
+        balance: 0,
+        tiles: [],
+        stocks: {},
+      });
+    });
+  });
+
+  describe("credit", () => {
+    it("should add the specified amount to the account balance", () => {
+      const account = new Account();
+      account.credit(6000);
+      const { balance } = account.stats();
+
+      assert.strictEqual(balance, 6000);
+    });
+  });
+
+  describe("debit", () => {
+    it("should add the specified amount to the account balance", () => {
+      const account = new Account();
+      account.credit(10000);
+      account.debit(6000);
+      const { balance } = account.stats();
+
+      assert.strictEqual(balance, 4000);
+    });
+  });
+
+  describe("addTile", () => {
+    it("should add specified tile in the tile collection", () => {
+      const account = new Account();
+      account.addTile("2A");
+      const { tiles } = account.stats();
+
+      assert.deepStrictEqual(tiles, ["2A"]);
+    });
+  });
+
+  describe("removeTile", () => {
+    it("should remove a specified tile from the tile collection", () => {
+      const account = new Account();
+      account.addTile("2A");
+      account.addTile("9I");
+      account.removeTile("2A");
+      const { tiles } = account.stats();
+
+      assert.deepStrictEqual(tiles, ["9I"]);
+    });
+  });
+});
