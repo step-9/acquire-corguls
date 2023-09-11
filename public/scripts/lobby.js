06f78cdce9c04762b72443bf890886db3d2559b1
Utsab Bera <nCoder24@users.noreply.github.com>
2023-09-11 16:50:49 +0530

    #1|Utsab/Debu|add player-profile in lobby page

--- public/scripts/lobby.js
+++ public/scripts/lobby.js
@@ -4,19 +4,21 @@ const getPlayers = () => {
   return fetch("/players").then(res => res.json());
 };
 
-const renderPlayer = username => {
-  const playerElement = document.createElement("li");
+const renderPlayer = (username, playerElement) => {
+  const playerNameContainer = playerElement.querySelector(".name");
+  const playerNameElement = document.createElement("p");
 
-  playerElement.innerText = username;
-  playerElement.classList.add("player");
-
-  return playerElement;
+  playerElement.classList.add("joined");
+  playerNameElement.innerText = username;
+  playerNameContainer.append(playerNameElement);
 };
 
 const renderPlayers = players => {
   const playerSection = getPlayerSection();
-  const playerElements = players.map(renderPlayer);
-  playerSection.append(...playerElements);
+  players.forEach((username, index) => {
+    const playerElement = playerSection.children[index];
+    renderPlayer(username, playerElement);
+  });
 };
 
 const main = () => {
