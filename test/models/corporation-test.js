d81368388922ae075e2a5b4d3af699e6fce67971
Utsab Bera <nCoder24@users.noreply.github.com>
2023-09-15 23:31:12 +0530

    [#12] Utsab/Qasim | Add tests for dynamic pricing of corporations

--- test/models/corporation-test.js
+++ test/models/corporation-test.js
@@ -1,6 +1,7 @@
 const assert = require("assert");
 const { it, describe } = require("node:test");
 const { Corporation } = require("../../src/models/corporation");
+const { CORPORATION_TYPES } = require("../../src/constants");
 
 describe("Corporation", () => {
   describe("establish", () => {
@@ -10,6 +11,97 @@ describe("Corporation", () => {
       assert.ok(!phoenix.isActive);
       phoenix.establish();
       assert.ok(phoenix.isActive);
+    });
+  });
+
+  describe("stats", () => {
+    it("should give status of a corporation based on the tiles", () => {
+      const phoenix = new Corporation(CORPORATION_TYPES.large);
+      const stats = {
+        stocks: 25,
+        tiles: [],
+        isActive: false,
+        price: 0,
+        majority: 2000,
+        minority: 1000,
+      };
+
+      assert.deepStrictEqual(phoenix.stats(), stats);
+    });
+
+    it("price should be reasonable for tiles between 6-10", () => {
+      const tiles = new Array(7).fill();
+      const phoenix = new Corporation(CORPORATION_TYPES.large, tiles);
+      const stats = {
+        tiles,
+        stocks: 25,
+        isActive: false,
+        price: 800,
+        majority: 2000,
+        minority: 1000,
+      };
+
+      assert.deepStrictEqual(phoenix.stats(), stats);
+    });
+
+    it("price should be reasonable for tiles between 11-20", () => {
+      const tiles = new Array(12).fill();
+      const phoenix = new Corporation(CORPORATION_TYPES.large, tiles);
+      const stats = {
+        tiles,
+        stocks: 25,
+        isActive: false,
+        price: 900,
+        majority: 2000,
+        minority: 1000,
+      };
+
+      assert.deepStrictEqual(phoenix.stats(), stats);
+    });
+
+    it("price should be reasonable for tiles between 21-30", () => {
+      const tiles = new Array(28).fill();
+      const phoenix = new Corporation(CORPORATION_TYPES.large, tiles);
+      const stats = {
+        tiles,
+        stocks: 25,
+        isActive: false,
+        price: 1000,
+        majority: 2000,
+        minority: 1000,
+      };
+
+      assert.deepStrictEqual(phoenix.stats(), stats);
+    });
+
+    it("price should be reasonable for tiles between 31-40", () => {
+      const tiles = new Array(33).fill();
+      const phoenix = new Corporation(CORPORATION_TYPES.large, tiles);
+      const stats = {
+        tiles,
+        stocks: 25,
+        isActive: false,
+        price: 1100,
+        majority: 2000,
+        minority: 1000,
+      };
+
+      assert.deepStrictEqual(phoenix.stats(), stats);
+    });
+
+    it("price should be reasonable for tiles between 41+", () => {
+      const tiles = new Array(53).fill();
+      const phoenix = new Corporation(CORPORATION_TYPES.large, tiles);
+      const stats = {
+        tiles,
+        stocks: 25,
+        isActive: false,
+        price: 1200,
+        majority: 2000,
+        minority: 1000,
+      };
+
+      assert.deepStrictEqual(phoenix.stats(), stats);
     });
   });
 
